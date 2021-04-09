5d9b9d5b0b03b4ed355366a2a368bfe9d1728243
Tim Dalsing <tdalsing@vmware.com>
2021-04-09 16:45:41 -0400

    updated test

--- misc/send.sh
+++ misc/send.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-data="{\"id\":\"id-1\",\"name\":\"name-1\"}"
-curl -X POST -w "\n" -i -d "$data" -H "Authorization: Bearer $token" -H "Content-Type: application/json" http://localhost:8301/data
+send() {
+  echo "data = $1"
+  curl -X POST -w "\n" -i -d "$1" -H "Authorization: Bearer $token" -H "Content-Type: application/json" http://localhost:8301/data
+}
+
+send '{"id":"id-1","name":"name-1"}'
+send '{"id":"id-2","name":"name-2","invalidJwt":true}'
+send '{"id":"id-3","name":"name-3","invalidSign":true}'
